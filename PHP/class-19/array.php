3d45823146dcc0c259690dd5e36f1ffc2492d9be
Shoaib Ahned <shoaib1971@outlook.com>
2019-08-15 18:32:20 +0600

    array.php aaded class-19 created in branch master

---
+++ PHP/class-19/array.php
@@ -0,0 +1,9 @@
+<?php
+$customer_name = array("SALMAN", "FERdOUS", "TANJINA", "NUR NABI", "RAFEED");
+//echo $customer_name[2];
+
+for($i =0; $i<count($customer_name); $i++){
+
+    echo $customer_name[$i]."<br>";
+}
+?>
