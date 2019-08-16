e21b85b667ff571f29f9a8875c090371a17cf729
Shoaib Ahned <shoaib1971@outlook.com>
2019-08-16 12:31:05 +0600

    Two files internal_function.php and superglobals.php added in class-21

---
+++ PHP/class-21/internal_function.php
@@ -0,0 +1,15 @@
+<?php
+/* STRING FUNCTIONS */
+//strlen()
+
+//echo  strlen("Md. Shoaib Ahmed");
+
+//echo  str_word_count("Md. Shoaib Ahmed");
+
+//echo  strpos("Md. Shoaib Ahmed", 'S');
+
+//echo  strrev("Md. Shoaib Ahmed");
+
+//echo  str_replace("Shoaib", "Mahfuz","Md. Shoaib Ahmed");
+
+?>
---
+++ PHP/class-21/superglobals.php
@@ -0,0 +1,22 @@
+<?php
+/*SOME SUPERGLOBALS VARIABLES*/
+//$GLOBALS
+//$_SERVER
+//$_REQUEST
+//$_POST
+//$_GET
+/*
+//$GOLBALS
+$x =20;
+$y = 50;
+
+function multiplication (){
+    $GLOBALS['multi'] = $GLOBALS['x']*$GLOBALS['y'];
+}
+
+multiplication();
+echo $multi;
+*/
+
+
+?>
